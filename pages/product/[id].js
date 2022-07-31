b693c919876db62d0d15cc3a6802617cb39b2d64
MrBurp <andreass.ziegler@gmail.com>
2022-07-31 12:26:41 +0200

    dynamic page management ready

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,5 +1,25 @@
-export async function getServerSideProps(context) {
-  const id = context.query.id;
+//genereted only the path
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: {
+          id: "1",
+        },
+      },
+      {
+        params: {
+          id: "2",
+        },
+      },
+    ],
+    fallback: "blocking",
+  };
+}
+
+export async function getStaticProps(context) {
+  const id = context.params.id;
+
   return {
     props: {
       id: id,
